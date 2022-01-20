3a5db2a7b0f6ed0765cd24f4ccc458bf930d0999
Rezyia <fineladiesprotectress@gmail.com>
2022-01-20 17:14:59 +0100

    librairies junit & jupiter

---
+++ src/test/java/StockDAOTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockDAOTest {
+
+    @org.junit.jupiter.api.Test
+    void getStock() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void reduceSugarStock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decrementSmallCupStock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decrementLargeCupStock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reduceWaterStock() {
+    }
+}
