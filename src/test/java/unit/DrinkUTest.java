97768db796be6bb7310b23f7b522b1216badd869
Hugo Julien <hugo.julien@etu.univ-tours.fr>
2022-01-27 17:08:51 +0100

    Tests du modèle Drink

---
+++ src/test/java/unit/DrinkUTest.java
@@ -0,0 +1,36 @@
+package unit;
+
+import model.Drink;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DrinkUTest {
+    Drink drink;
+
+    @BeforeEach
+    void init(){
+        this.drink = new Drink(1, "Breuvage intriguant", "Une boisson bizarre", 50.00d);
+    }
+
+    @Test
+    void testGetName(){
+        Assertions.assertEquals("Breuvage intriguant", drink.getName());
+    }
+
+    @Test
+    void testGetDescription(){
+        Assertions.assertEquals("Une boisson bizarre", drink.getDescription());
+    }
+
+    @Test
+    void testGetPrice(){
+        Assertions.assertEquals(50.00d, drink.getPrice());
+    }
+
+    @Test
+    void setName(){
+        drink.setName("Boisson perturbante");
+        Assertions.assertEquals("Boisson perturbante", drink.getName());
+    }
+}
