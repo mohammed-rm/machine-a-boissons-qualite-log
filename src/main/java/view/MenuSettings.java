3c76ebf3ad2ca0ed844b9bf0bbcfb11fbc449631
mohammed_rm <mohammed.rmich@etu.univ-tours.fr>
2022-01-27 16:01:43 +0100

    settings panel done

--- src/main/java/view/MenuSettings.java
+++ src/main/java/view/MenuSettings.java
@@ -9,6 +9,8 @@ public class MenuSettings extends Component {
 	private JComboBox<String> comboBox;
 	private JLabel labLanguage;
 	private InternalPanel pan;
+	
+	private JLabel notFinished;
 
 	/**
 	 * Default constructor
@@ -16,6 +18,7 @@ public class MenuSettings extends Component {
 	public MenuSettings() {
 		comboBox = new JComboBox<String>();
 		labLanguage = new JLabel("Select a language");
+		notFinished = new JLabel("To Be Continued...");
 		pan = new InternalPanel();
 		pan.setLocation(0,0);
 	}
@@ -34,8 +37,11 @@ public class MenuSettings extends Component {
 		comboBox.addItem("En");
 		labLanguage.setFont(new Font("Times New Roman", Font.BOLD, 16));
 		labLanguage.setBounds(10, 14, 130, 20);
+		notFinished.setFont(new Font("Script MT Bold", Font.ITALIC, 40));
+		notFinished.setBounds(140, 250, 500, 30);
 
 		pan.add(labLanguage);
+		pan.add(notFinished);
 		pan.add(comboBox);
 		
 		return pan;
